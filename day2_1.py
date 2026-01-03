79eeb6e47fa90711d2a19f2b676ee21ae4763988
austin <51234553+Austin-Senna@users.noreply.github.com>
2026-01-03 03:14:47 -0500

    day5 done

--- day2_1.py
+++ day2_1.py
@@ -2,34 +2,79 @@ maxNum = 0
 
 inputs = []
 
-def check(n):
-    s = str(n)
-    if s[:len(s)//2] != s[len(s)//2:]:
-        return False
-    return True
+def count_invalid(left, right, n):
+    # count = 0 
+    res = []
     
+    if left[:n//2] == right[:n//2]:
+        # print(left[:n//2], right[:n//2])
+        # print(int(left[:n//2]))
+        # print(int(left[n//2:]))
+        if int(left[:n//2]) >= int(left[n//2:]):
+            # print("YES")
+            
+            if int(right[:n//2]) <= int(right[n//2:]): 
+                # print("NO")
+                res.append(left[:n//2] * 2)
+        return res
+
+    if int(left[:n//2]) >= int(left[n//2:]):
+        res.append(left[:n//2] * 2)
+
+    curr = int(left[:n//2])
+    right_max = int(right[:n//2])
+
+    while curr < right_max-1:
+        curr += 1
+        res.append(str(curr) * 2)
+
+    if int(right[:n//2]) <= int(right[n//2:]):
+        res.append(right[:n//2] * 2)
+    
+    return res
+
+
+    
+
 inputs = input().split(",")
-inputsChanged = []
+inputs_changed = []
 
 for input in inputs:
     if len(input) == 0:
         continue
-    lo, hi = map(int, input.split("-"))  
-    inputsChanged.append((lo, hi))
-    if hi > maxNum:
-        maxNum = hi    
+    lo, hi = input.split("-")
+    inputs_changed.append((lo, hi))
+ 
+print(inputs_changed)
 
-nums = [0] * (maxNum + 1)
+tot_res = []
+for input_range in inputs_changed:
+    # print(input_range)
+    left = input_range[0]
+    right = input_range[1]
+    while len(left) < len(right):
+        if len(left) % 2 == 1:
+            left = "1"+ "0" * len(left)
+            continue
+        
+        curr_max = "9" * len(left)
+        # print(curr_max, left)
+        curr_extension = count_invalid(left, curr_max, len(left))
+        # print(curr_extension)
+        tot_res.extend(curr_extension)
+        # print(tot_res)
+        
+        left = "1" + "0" * len(left)
+        # print(curr_max)
+    if len(left) % 2 == 0:
+        curr_extension = count_invalid(left, right, len(left))
+        tot_res.extend(curr_extension)
+    # print(curr_extension)
+    # print(tot_res)
+print(tot_res)
+tot_res = map(int, tot_res)
+print(sum(tot_res))
 
-for i in range(maxNum + 1):
-    if len(str(i)) % 2 == 1:
-        nums[i] = nums[i-1]
-    nums[i] = nums[i-1] + 1 if check(i) else nums[i-1]
-    if i % 1000 == 0:
-        print(f"Processed up to {i}")
 
-currSum = 0
-for lo, hi in inputsChanged:
-    currSum += nums[hi] - nums[lo-1]
+# for changed in inputs_changed:
 
-print(currSum)
